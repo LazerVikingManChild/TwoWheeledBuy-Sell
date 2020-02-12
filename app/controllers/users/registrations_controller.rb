a94f9538684bfb8b4db35f18333b0400f81456a8
“Christopher <christopher.brum.comssh-keygen -t rsa -C christopher.brum.com>
2020-02-12 12:06:54 -0500

    avatar added to edit profile

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :location, :user_quote, :user_description])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :location, :user_quote, :user_description, :avatar])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :location, :user_quote, :user_description])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :location, :user_quote, :user_description, :avatar])
   end
 
   # The path used after sign up.
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts, :dependent => :delete_all
+  has_one_attached :avatar
 end
